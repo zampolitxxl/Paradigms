871e6faa11c674f53844f75441a69a1ed36dc19b
kirilltimofeev <sushi-maker@yandex.ru>
2024-01-20 21:16:32 +0300

    HwSem2Done

--- src/main/java/org/example/Seminar1.java
+++ src/main/java/org/example/Seminar1.java
@@ -3,10 +3,10 @@ package org.example;
 public class Seminar1 {
 
     //императивный стиль
-    public static void sortListImperative(int[] numbers){
+    public static void sortListImperative(int[] numbers) {
         for (int i = 0; i < numbers.length - 1; i++) {
-            for(int j = 0; j < numbers.length - i - 1; j++) {
-                if(numbers[j + 1] > numbers[j]) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j + 1] > numbers[j]) {
                     int swap = numbers[j];
                     numbers[j] = numbers[j + 1];
                     numbers[j + 1] = swap;
@@ -15,7 +15,7 @@ public class Seminar1 {
         }
     }
 
-    //декларативный  стиль
+    /*декларативный  стиль
     public static void declarativeSortListImperative(int[] numbers){
         sortMaxMin(numbers);
         print(numbers);
@@ -25,4 +25,7 @@ public class Seminar1 {
 
 
 
+}
+
+     */
 }
---
+++ src/main/java/org/example/Seminar2.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Seminar2 {
+
+    public static void main(String[] args) {
+
+        int n = 11;
+
+        for( int i = 1; i<n;i++)
+
+        {
+            System.out.println("1*"+i+"="+i);
+        }
+
+
+    }
+
+
+
+}
